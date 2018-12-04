88b4ae83ae36df096b934fab0446cf14a7bb4f40
18382268894 <qq1005914310@live.com>
2018-12-05 04:20:47 +0800

    控制器

---
+++ controller/loginController.go
@@ -0,0 +1,16 @@
+/**
+*FileName: controller
+*Create on 2018/12/5 上午4:12
+*Create by mok
+*/
+
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Login(c * gin.Context){
+	c.HTML(http.StatusOK,"account",nil)
+}
